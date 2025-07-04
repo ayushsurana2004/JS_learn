908405c9d0b73dd95ed3e6a11e65b2f5aaf88c4e
Ayush Surana <ayush.surana2004@gmail.com>
2025-07-04 20:41:43 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+let score="33"
+console.log(typeof(score)).   //n is small
+let valInNum=Number(score)   //N is capital
+console.log(valInNum)
+console.log(typeof valInNum)
+
+//if it is a string with char then type of valInNum will be number but value will be NaN
+//33=>33
+//33abc=>NaN
+
+//1=>true
+//0=>false
+//""=>false
+//"Ayush"=>true
