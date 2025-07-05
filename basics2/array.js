4baf5428e6799f4e849a9bb0a4a2a306cb3258bf
Ayush Surana <ayush.surana2004@gmail.com>
2025-07-05 21:24:51 +0000

    arrays

---
+++ basics2/array.js
@@ -0,0 +1,24 @@
+const myArr=new Array(1,2,3,4,5,6)
+myArr.push(7)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
+
+/*
+unshift : to add value at beginning
+shift : to remove first value
+includes()
+join : adds all elements of array to strings
+slice : to print subsection
+splice : print subsection but removes the portion from original array
+concat : adds two arrays
+
+const arr1,arr2
+const arr3=[...arr1,...arr2]
+//it is like concat
+
+flat(n) : multi dimensional array to 1d upto n depths. n can be infinity to make it perfectly flat
+
+Array.from("Ayush") : convert it to array 
+Array.of(num1,num2,num3) : convert to array
+*/
